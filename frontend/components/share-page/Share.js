cfbe62e30cb28b5d68a8bb41d51ae2928e9b6d08
Ben Vinnick <benvinnick@gmail,com>
2021-08-22 01:58:31 -0700

    feat(frontend): completed frontend

--- frontend/components/share-page/Share.js
+++ frontend/components/share-page/Share.js
@@ -1,6 +1,6 @@
 import styles from './Share.module.scss'
 
-const Share = ({ url}) => {
+const Share = ({ url }) => {
 	return (
 		<div className={styles.share}>
 			<a href="fb-messenger://share/?link= https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fsharing%2Freference%2Fsend-dialog&app_id=123456789">Send In Messenger</a>
